8c06368dd00d2ff4b7980697e6dff3c064bcb3d7
wongsathonth <wongsathon.t@kkumail.com>
2022-11-28 15:59:11 +0700

    5555+

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("wongsathon thongin");
+Console.WriteLine("6534501017");
+Console.WriteLine("wongsathon.t@kkumail.com");
